503335574a4ae09074f43f070f994d9eaa920bc8
vigneesh-22 <vicky06237@gmail.com>
2026-08-04 00:16:21 +0530

    Sync LeetCode solutions

---
+++ leetcode/medium/0909_stone_game.java
@@ -0,0 +1,10 @@
+// LeetCode 909: Stone Game
+// Difficulty: Medium | Language: java | Accepted: 2026-08-03T18:45:51+00:00
+// https://leetcode.com/problems/stone-game/
+//
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
